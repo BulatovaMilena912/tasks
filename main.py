ff2d44d993a67fb3c2f56435af318708b222a105
MimiRu <bulatovamilena90@gmail.com>
2023-09-28 00:56:32 +0300

    players added

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-# Бот загадывает число от 1 до 100 (сообщить об этом пользователю). Запрашивать от пользователя число до тех пор, пока оно не будет равно числу, которое загадал бот. Если число пользователя меньше или больше загаданного, то сообщить об этом пользователю. В конце поблагодарить пользователя за игру.
-# После правильного ответа спросить пользователя, хочет ли он продолжить играть или нет. Если да, то бот загадывает новое число, иначе завершить игру.
+# Если игрок угадает число, то его рейтинг увеличивается на N очков. Далее спросить каждого игрока, хочет ли он продолжить играть. Если кто-то не хочет, то завершить игру.
+
 from player import Player
 import random
+
+
+def registor():  # функция регистрации
+    players = []  # список игроков
+    amount = int(input("Сколько игроков будут играть: "))  # количество игроков
+    for i in range(amount):
+        name = input("Введите ваше имя: ")  # имя игрока
+        player = Player(name)  # экземпляр класса
+        players.append(player)  # экз в списке
+        print()  # заполнение списка
+    return players  # возвращает  список
+
+
 if __name__ == "__main__":
+    players: list[Player] = registor()  # экземпляр хранящий список игроков
     wanna_play = True
-    while wanna_play:
+    while wanna_play:  # число бота
         bot_number = random.randrange(1, 100)
         print("Бот задал число")
-        while True:
-            user_number = int(input("Введите ваше число: "))
-            if bot_number > user_number:
-                print("Ваше число меньше")
-            elif user_number > bot_number:
-                print("Ваше число больше")
-            else:
-                print("Отгадано правильно! Спасибо за игру!")
-                wanna_play = True if input(
-                    "Вы хотите играть дальше? (Да/нет)?").lower() == "да" else False
-                break
+        while True:  # отгадывание числа
+            for p in players:
+                print(f"Игрок {p.nickname} в игре. Ваша очередь!")
+                user_number = int(input("Введите ваше число: "))
+                if bot_number > user_number:
+                    print("Ваше число меньше")
+                elif user_number > bot_number:
+                    print("Ваше число больше")
+                else:
+                    print("Отгадано правильно! Спасибо за игру!")
+                    wanna_play = True if input(
+                        "Вы хотите играть дальше? (Да/нет)?").lower() == "да" else False
+                    break
